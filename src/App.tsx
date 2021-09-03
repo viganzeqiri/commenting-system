aeb9104b4ac186ac1c75272a01c5551ff4ad3e79
viganzeqiri <viganzeqirii@gmail.com>
2021-09-03 14:51:48 +0200

    added chakra ui library, created custom theme and started adding Post components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import "./App.css";
 
 export function App() {
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import { App } from "./App";
+import { theme } from "./theme";
+
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
